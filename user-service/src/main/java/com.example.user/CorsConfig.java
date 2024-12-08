d40c9b21dac2f177a8cf12346baf64302f5d34ab
Hamras Hilar A <hamrashilar@gmail.com>
2024-12-08 07:06:57 +0000

    Update CorsConfig.java

--- user-service/src/main/java/com.example.user/CorsConfig.java
+++ user-service/src/main/java/com.example.user/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://:30003")
+                        .allowedOrigins("http://:80")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
