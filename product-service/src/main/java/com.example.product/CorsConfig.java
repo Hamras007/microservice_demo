a5e3d31babebd02e1b1db16f1eb10d9cf22dda9e
Hamras Hilar A <hamrashilar@gmail.com>
2024-12-09 09:17:17 +0000

    Update CorsConfig.java

--- product-service/src/main/java/com.example.product/CorsConfig.java
+++ product-service/src/main/java/com.example.product/CorsConfig.java
@@ -13,22 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://package com.example.product;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://k8s-nlb-1388490701.ap-south-1.elb.amazonaws.com")
+                        .allowedOrigins("http://dnsname")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -36,12 +21,5 @@ public class CorsConfig {
         };
     }
 }
-")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-}
+
 
